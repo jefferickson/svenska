de7e449a4daad0f4f07f9ce05e38461727f0c5c3
Jeff Erickson <jeff@erick.so>
2015-03-22 18:21:18 -0400

    more dev on the parser. can read/write to shelve now

--- svenska_parser.py
+++ svenska_parser.py
@@ -4,7 +4,9 @@
 import csv
 import sys
 import argparse
+import shelve
 
+# Import our parts of speech classes
 sys.path.append('./parts_of_speech/')
 import adjective as adj
 import adverb as adv
@@ -14,8 +16,15 @@ import word
 
 
 class Svenska_Parser:
+    '''This is the main parser class. It can read CSV word lists and create word objects.'''
+
+    # Class attrs
+    ## key for word list in a shelve
+    key_word_list = 'word_list'
 
     def __init__(self, dispatcher_override = None):
+        self.parsed_words = {}
+        self.words_from_shelve = {}
         
         if not dispatcher_override:
             self.dispatcher = {
@@ -32,7 +41,6 @@ class Svenska_Parser:
 
     def parse_word_list(self, files, delimiter = ','):
         '''For each line in each file, dispatch to proper class definition.'''
-        self.parsed_words = {}
 
         for a_file in files:
             with open(a_file, 'r') as f:
@@ -41,6 +49,18 @@ class Svenska_Parser:
                     part_of_speech, word, *definition = row
                     if row[0][0] == '#': continue # skip comments
                     self.parsed_words.setdefault(word, []).append(self.dispatcher[part_of_speech](word, definition))
+
+    def shelve_word_list(self, shelve_file):
+        '''Shelve the parsed word list.'''
+
+        with shelve.open(shelve_file, 'c') as shelf:
+            shelf[Svenska_Parser.key_word_list] = self.parsed_words
+
+    def unshelve_word_list(self, shelve_file):
+        '''Un-shelve word objects from file.'''
+
+        with shelve.open(shelve_file, 'r') as shelf:
+            self.words_from_shelve = shelf[Svenska_Parser.key_word_list]
 
 
 if __name__ == '__main__':
@@ -52,8 +72,10 @@ if __name__ == '__main__':
 
     cmd_line_args = args.parse_args()
 
+    # If files exist to be parsed, let's parse them
     if cmd_line_args.parse_word_list:
         parser = Svenska_Parser()
         parser.parse_word_list(cmd_line_args.files)
-
-        print(parser.parsed_words)
+        # And if an output file is specified, let's store the shelve of parsed words there
+        if cmd_line_args.o:
+            parser.shelve_word_list(cmd_line_args.o)
